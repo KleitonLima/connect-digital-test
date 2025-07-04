15cb2e0361b402067dda2cc1e6d21d4cdb246620
Kleiton Lima <kleiton.mini@gmail.com>
2025-07-04 14:04:49 -0300

    feat(main): enhance bootstrap function with global prefix, validation pipes, and Swagger configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { swaggerConfig } from './config/swagger.config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { setupDbConnection } from './database/connection.db';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
+  app.setGlobalPrefix('api/v0');
+  app.useGlobalPipes(new ValidationPipe());
+
+  swaggerConfig(app);
+
+  await setupDbConnection();
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
