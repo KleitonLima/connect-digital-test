d013e05e2937238cddec9ccae1aa8b145e50b2fa
Kleiton Lima <kleiton.mini@gmail.com>
2025-07-05 14:11:47 -0300

    feat(app): implement receiveWebhook method to handle incoming webhook data

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { ENVCONFIG } from 'src/config/env.config';
+import { WebhookPayloadDto } from './dto/payload-webhook.dto';
+
 @Injectable()
 export class AppService {
   getAppStatus(): string {
     return `Server is running! 🚀\n Please check <a href="http://localhost:${ENVCONFIG.PORT}/api/v0/docs">http://localhost:${ENVCONFIG.PORT}/api/v0/docs</a> for Swagger docs...`;
+  }
+
+  receiveWebhook(data: WebhookPayloadDto) {
+    console.log('Received webhook data:', data);
+    return {
+      message: 'Webhook received successfully!',
+      status: 'success',
+      timestamp: new Date(),
+    };
   }
 }
