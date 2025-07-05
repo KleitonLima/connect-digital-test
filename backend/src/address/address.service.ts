49f1e4b9b8cc86d8571e3a9e833d5675110a30bc
Kleiton Lima <kleiton.mini@gmail.com>
2025-07-05 15:53:36 -0300

    feat(address): implement create method to add new addresses

--- backend/src/address/address.service.ts
+++ backend/src/address/address.service.ts
@@ -1,13 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateAddressDto } from './dto/create-address.dto';
+import { Address } from './entities/address.entity';
 
 @Injectable()
 export class AddressService {
-  create(createAddressDto: CreateAddressDto) {
-    return 'This action adds a new address';
+  constructor(
+    @InjectRepository(Address)
+    private readonly addressRepository: Repository<Address>,
+  ) {}
+
+  async create(createAddressDto: CreateAddressDto): Promise<Address> {
+    const {
+      street,
+      streetNumber,
+      complement,
+      zipCode,
+      neighborhood,
+      city,
+      state,
+      country,
+    } = createAddressDto;
+
+    const address = this.addressRepository.create({
+      street,
+      street_number: streetNumber,
+      complement: complement || null,
+      zip_code: zipCode,
+      neighborhood,
+      city,
+      state,
+      country,
+    });
+
+    return await this.addressRepository.save(address);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} address`;
+  async findOne(id: number): Promise<Address | null> {
+    return await this.addressRepository.findOne({ where: { id } });
   }
 }
