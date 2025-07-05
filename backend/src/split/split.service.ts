6b12932c6e272106cff04a9eb4a6411a62db4ebd
Kleiton Lima <kleiton.mini@gmail.com>
2025-07-05 15:54:05 -0300

    feat(split): implement create method for split management

--- backend/src/split/split.service.ts
+++ backend/src/split/split.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateSplitDto } from './dto/create-split.dto';
+import { Split } from './entities/split.entity';
 
 @Injectable()
 export class SplitService {
-  create(createSplitDto: CreateSplitDto) {
-    return 'This action adds a new split';
+  constructor(
+    @InjectRepository(Split)
+    private readonly splitRepository: Repository<Split>,
+  ) {}
+
+  async create(createSplitDto: CreateSplitDto): Promise<Split> {
+    const { recipientId, amount, netAmount } = createSplitDto;
+
+    const split = this.splitRepository.create({
+      recipient_id: recipientId,
+      amount,
+      net_amount: netAmount,
+    });
+
+    return await this.splitRepository.save(split);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} split`;
+  async findOne(id: number): Promise<Split | null> {
+    return await this.splitRepository.findOne({ where: { id } });
   }
 }
