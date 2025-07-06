9d0de002360b425b4b485ef6087c86f1da55927a
Kleiton Lima <kleiton.mini@gmail.com>
2025-07-06 01:15:27 -0300

    fix(migration): correct path to TypeORM configuration in migration generation script

--- backend/src/database/scripts/db-migration-generate.ts
+++ backend/src/database/scripts/db-migration-generate.ts
@@ -12,7 +12,7 @@ async function run() {
   const migrationName = argv.name;
 
   exec(
-    `typeorm-ts-node-commonjs -d ./src/database/config/typeorm.db.ts migration:generate ./src/database/migrations/${migrationName}`,
+    `typeorm-ts-node-commonjs -d ./src/database/typeorm.db.ts migration:generate ./src/database/migrations/${migrationName}`,
     (error, stdout, stderr) => {
       if (error) {
         console.error(`Error: ${error.message}`);
