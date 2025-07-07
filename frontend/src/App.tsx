634ee97c11ec1e097d045d44ed29a501dfa30e13
Kleiton Lima <kleiton.mini@gmail.com>
2025-07-07 11:05:00 -0300

    feat: update App component to render SaleCheckout page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
-import Home from './pages/Home/Home';
+import SaleCheckout from './pages/Home/SaleCheckout';
 
 function App() {
-  return <Home />;
+  return <SaleCheckout />;
 }
 
 export default App;
