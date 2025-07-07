1fc171350dc203da1f71c5fad33b745edb79c238
Kleiton Lima <kleiton.mini@gmail.com>
2025-07-07 14:29:22 -0300

    feat(payment): add async keyword to create method and implement generatePaymentIntentPix endpoint

--- backend/src/payment/payment.controller.ts
+++ backend/src/payment/payment.controller.ts
@@ -1,13 +1,23 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { CreatePaymentDto } from './dto/create-payment.dto';
+import { GeneratePaymentIntentDto } from './dto/generate-payment-intent.dto';
 
 @Controller('payment')
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
   @Post()
-  create(@Body() createPaymentDto: CreatePaymentDto) {
+  async create(@Body() createPaymentDto: CreatePaymentDto) {
     return this.paymentService.create(createPaymentDto);
+  }
+
+  @Post('payments/pix')
+  async generatePaymentIntentPix(
+    @Body() generatePaymentIntentDto: GeneratePaymentIntentDto,
+  ) {
+    return this.paymentService.generatePaymentIntentPix(
+      generatePaymentIntentDto,
+    );
   }
 }
