2663788a9fbe71accb2196ecc26a710674f163f8
Kleiton Lima <kleiton.mini@gmail.com>
2025-07-07 16:48:39 -0300

    refactor(payment): remove unused create payment method from PaymentController

--- backend/src/payment/payment.controller.ts
+++ backend/src/payment/payment.controller.ts
@@ -1,16 +1,10 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { PaymentService } from './payment.service';
-import { CreatePaymentDto } from './dto/create-payment.dto';
 import { GeneratePaymentIntentDto } from './dto/generate-payment-intent.dto';
 
 @Controller('payments')
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
-
-  @Post()
-  async create(@Body() createPaymentDto: CreatePaymentDto) {
-    return this.paymentService.create(createPaymentDto);
-  }
 
   @Post('pix')
   async generatePaymentIntentPix(
